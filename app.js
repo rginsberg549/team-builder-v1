23a7ffeeb39b20d057947ae1cc29399b4296f11e
rginsberg <rginsberg549@gmail.com>
2020-09-07 18:08:03 -0700

    adding app to github

---
+++ app.js
@@ -0,0 +1,87 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+
+const questions = [
+    {
+        type: "list",
+        name: "role",
+        default: 'Manager',
+        choices: ['Manager', 'Engineer', 'Intern', new inquirer.Separator(), 'Done'],
+        message: "Choose a role to add to your team.",
+
+    },
+    {
+        type: "input",
+        name: "name",
+        default: "Test Name",
+        message: "What is their name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        default: 1,
+        message: "What is their ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        default: "test@test.com",
+        message: "What is their email?"
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        default: 100,
+        message: 'Enter Office Number',
+        when: (answers) => answers.role === 'Manager'
+    }
+]
+
+function promptUser() {
+  return inquirer.prompt(questions);
+}
+
+
+
+promptUser()
+  .then(function(data) {
+      render(data);
+    return console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
